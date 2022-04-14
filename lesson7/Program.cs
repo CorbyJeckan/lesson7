9497812330da299c4ae2120d3f2c659bf5cf23a2
Евгений Савельев <curtisjeckan@mail.ru>
2022-04-14 16:01:57 +0500

    дз7создал простое приложение

--- lesson7/Program.cs
+++ lesson7/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string secret = "some secret password";
+            Console.WriteLine("Enter password:");
+            string input = Console.ReadLine();
+            if (input == secret)
+            {
+                Console.WriteLine("Welcome!");
+            }
+
+            Console.ReadKey(true);
+                      
+        }
+    }
+}
